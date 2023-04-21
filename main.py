2905ae98b66e510d5d63b84d003716c7ae25fbce
Camilo Jaramillo <camilojd77@gmail.com>
2023-04-21 12:09:27 -0500

    Se actualiza main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-""" Supongamos que estamos desarrollando un programa que calcula el costo de envío de paquetes y el total a pagar 
-para una tienda en línea. Los costos de envío varían según el destino del paquete y el método 
-de envío elegido (por ejemplo, envío estándar, envío express, etc.). Además, es posible que 
-en el futuro se agreguen nuevos métodos de envío o destinos. Queremos asegurarnos de que nuestro 
-programa sea fácilmente extensible y que se pueda agregar nuevos cálculos de costo sin tener que 
-cambiar el código existente. """
+""" 
+Se necesita desarrollar un programa que calcula el costo de envío de paquetes y el total a pagar para una 
+tienda en línea. Los costos de envío varían según el destino del paquete y el método de envío elegido 
+(envío estándar, envío express y envío premium). Además, es posible que en el futuro se agreguen nuevos 
+métodos de envío o destinos. Queremos asegurarnos de que nuestro programa sea fácilmente extensible y 
+que se pueda agregar nuevos cálculos de costo sin tener que cambiar el código existente.  """
 
 from envio import CalculoEnvio, EnvioEstandar, EnvioExpress, EnvioPremium
 from pedido import Pedido
