7524a130a22293e196b7567c9ca2ce2102bfe5db
Camilo Jaramillo <camilojd77@gmail.com>
2023-04-29 08:33:07 -0500

    Se actualiza main.py para que solo detenga la ejecución cuando se indique

--- main.py
+++ main.py
@@ -8,9 +8,38 @@ que se pueda agregar nuevos cálculos de costo sin tener que cambiar el código 
 from envio import CalculoEnvio, EnvioEstandar, EnvioExpress, EnvioPremium
 from pedido import Pedido
 
-pedido = Pedido(destino="Colombia", valorCompra=100.00)
-envio = EnvioPremium()
-costo = envio.calcular_envio(pedido)
-totalPago = costo + pedido.valorCompra
-print(f"Costo de envío: {costo}")
-print(f"Total a pagar: {totalPago}")
+continuar = True
+while (continuar):
+    print("Ingrese el destino del pedido: ")
+    destinoSleccionado = input()
+    print("Ingrese valor compra: ")
+    valorCompraIngresado = input()
+    print("Ingrese el número del método de envío: 1-Estandar, 2-Express, 3-Premium")
+    metodoEnvio = input()
+
+    if metodoEnvio == "1":
+        metodoEnvio = EnvioEstandar()
+    elif metodoEnvio == "2":
+        metodoEnvio = EnvioExpress()
+    elif metodoEnvio == "3":
+        metodoEnvio = EnvioPremium()
+    else:
+        print("Método de envío no válido")
+    
+    valorCompraIngresado = float(valorCompraIngresado)
+
+    print(f"Destino: {destinoSleccionado}")
+    print(f"Método de envío: {metodoEnvio.__class__.__name__.replace('Envio','Envío ').replace('()','')}")
+    print(f"Valor Compra: {valorCompraIngresado} dólares")
+
+    pedido = Pedido(destino=destinoSleccionado, valorCompra=valorCompraIngresado)
+    envio = metodoEnvio
+    costo = envio.calcular_envio(pedido)
+    totalPago = costo + pedido.valorCompra
+    print(f"Costo de envío: {costo} dólares")
+    print(f"Total a pagar: {totalPago} dólares")
+    
+    print("¿Desea Continuar (SI/NO)? ")
+    deseaContinuar = input()
+    if (deseaContinuar.lower() != "si"):
+        break
